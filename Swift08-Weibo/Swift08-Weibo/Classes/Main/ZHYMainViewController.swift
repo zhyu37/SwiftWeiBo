2e7572f8939a8641279d7a8157eeb7c42088328d
zhyu <466096851@qq.com>
2016-04-25 22:41:30 +0800

    初始化项目

---
+++ Swift08-Weibo/Swift08-Weibo/Classes/Main/ZHYMainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ZHYMainViewController.swift
+//  Swift08-Weibo
+//
+//  Created by zhyu on 16/4/25.
+//  Copyright © 2016年 zhyu. All rights reserved.
+//
+/// commond + j (定位到目录结构) -> commond + n ()创建文件
+import UIKit
+
+class ZHYMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
